8d3bbe72c303eb916f8929dcc2dff3d8ad90876b
João Vitor Hobus <69995779+miojo-dev@users.noreply.github.com>
2026-05-17 17:36:40 -0300

    Add simple linked list creation and manipulation

--- main.pas
+++ main.pas
@@ -122,6 +122,103 @@ begin
 end;
 
 // lista dupla
+procedure CriarListaSimples(var lista : TNode);
+begin
+    lista := nil;
+end;
+
+procedure AdicionarSimples(var lista : TNode; info : TInfo);
+var aux, anterior, atual : TNode;
+begin
+    new(aux);
+    if aux = nil then
+    begin
+        write('Memoria cheia!'); readkey;
+    end
+    else
+    begin
+        aux^.info := info;
+        aux^.prox := nil;
+        
+        if (lista = nil) or (info < lista^.info) then
+        begin
+            aux^.prox := lista;
+            lista := aux;
+        end
+        else
+        begin
+            anterior := lista;
+            atual := lista^.prox;
+            
+            while (atual <> nil) and (info > atual^.info) do
+            begin
+                anterior := atual;
+                atual := atual^.prox;
+            end;
+            
+            aux^.prox := atual;
+            anterior^.prox := aux;
+        end;
+    end;
+end;
+
+procedure RemoverSimples(var lista : TNode; info : TInfo);
+var anterior, atual : TNode;
+begin
+    if lista = nil then
+    begin
+        write('Lista vazia!'); readkey;
+    end
+    else
+    begin
+        anterior := nil;
+        atual := lista;
+        
+        while (atual <> nil) and (atual^.info <> info) do
+        begin
+            anterior := atual;
+            atual := atual^.prox;
+        end;
+        
+        if atual = nil then
+        begin
+            write('Elemento não encontrado!'); readkey;
+        end
+        else
+        begin
+            // Era o primeiro da listaa?
+            if anterior = nil then
+                lista := atual^.prox
+            else
+                anterior^.prox := atual^.prox;
+            
+            writeln('Elemento ', atual^.info, ' removido!');
+            dispose(atual);
+            readkey;
+        end;
+    end;
+end;
+
+function ContarListaSimples(var lista : TNode) : byte;
+var aux : TNode;
+    i : byte;
+begin
+    i := 0;
+    
+    if lista <> nil then
+    begin
+        aux := lista;
+        
+        while aux <> nil do
+        begin
+            i := i + 1;
+            writeln(i, ' - ', aux^.info);
+            aux := aux^.prox;
+        end
+    end;
+    
+    ContarListaS := i;
+end;
 
 begin
     option := 1;
